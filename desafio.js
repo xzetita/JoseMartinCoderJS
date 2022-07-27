d878d6aac6ab34a82f08fe749bbd02efd45072da
xzetita <josemmartin99@gmail.com>
2022-07-27 10:32:04 -0300

    Desafío

---
+++ desafio.js
@@ -0,0 +1,13 @@
+let ingreso = prompt ("Su nombre es?");
+while (ingreso != "ESC"){
+    switch (ingreso) {
+        case "Jose": 
+        alert ("Hola, bienvenido!");
+        break;
+
+        default: 
+        alert ("Saludos cordiales");
+        break;
+    }
+    entrada = prompt("Ingresar un nombre")
+}
