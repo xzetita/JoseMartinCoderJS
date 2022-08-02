96e6600ef6dada2ce912056154d677da025f9e72
xzetita <josemmartin99@gmail.com>
2022-08-02 06:43:17 -0300

    Olvidé de hacer el commit

--- desafio.js
+++ desafio.js
@@ -1,13 +1,33 @@
-let ingreso = prompt ("Su nombre es?");
-while (ingreso != "ESC"){
-    switch (ingreso) {
-        case "Jose": 
-        alert ("Hola, bienvenido!");
-        break;
-
-        default: 
-        alert ("Saludos cordiales");
-        break;
+let BaseAlumnos = [];
+let promedio = 0;
+class Alumno {
+    constructor (nombre, apellido,calificacion){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.calificacion=calificacion;
     }
-    entrada = prompt("Ingresar un nombre")
+}
+
+/* Boton agregar alumno*/
+function agregar () {
+    let nombreCaptura= document.getElementById("nombre").value;
+    let apellidoCaptura=document.getElementById("apellido").value;
+    let calificacionCaptura= parseFloat(document.getElementById("calificacion").value);
+    let nuevoAlumno= new Alumno (nombreCaptura,apellidoCaptura,calificacionCaptura);
+    guardar(nuevoAlumno);
+}
+function guardar(nuevoAlumno){
+    BaseAlumnos.push(nuevoAlumno);
+}
+/* Boton promedio de alumnos*/
+function cantidadAlumnos (){
+    console.log(BaseAlumnos.length);
+}
+/* Boton promedio alumnos*/
+function promedioAlumnos(){
+    let i = 0;
+    for (i ; BaseAlumnos.length >= i;i++){
+promedio += BaseAlumnos[i].calificacion;
+    }
+    console.log("El promedio de las calificaciones es " + promedio/BaseAlumnos.length)
 }
